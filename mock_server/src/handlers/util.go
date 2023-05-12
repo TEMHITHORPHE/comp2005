6554e1d4cde7d2617f52c2d79f76df56ef6929fd
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-05-12 09:04:01 +0100

    json response utili

--- mock_server/src/handlers/util.go
+++ mock_server/src/handlers/util.go
@@ -1,12 +1,21 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 var pr = fmt.Println
 
-func sendJSONRespose(w http.ResponseWriter) {
+// How do you say "Allow only list of structs!!"?
+// type ListOfStructs interface {
+// 	struct
+// }
 
+func sendJSONRespose(response http.ResponseWriter, json_data any) {
+
+	json_response, _ := json.Marshal(json_data)
+	response.Header().Add("content-type", "application/json")
+	response.Write(json_response)
 }
