d568599facbde810eccbf0fd979b3bf4f862ad15
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-05-12 08:21:56 +0100

    Repo Init [late though]

---
+++ go.mod
@@ -0,0 +1,3 @@
+module phxlabs/m/comp2005/automated_testing
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+var pr = fmt.Println
+
+func main() {
+	pr("Welcome ::: Report Automated Software Testing ... ")
+
+	// http.HandleFunc()
+}
