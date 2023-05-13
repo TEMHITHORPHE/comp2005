73119b40ce88960bfb59b900c80b3be3f21f75b9
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-05-14 00:13:40 +0100

    Added Models

---
+++ src/models/models.go
@@ -0,0 +1,29 @@
+package models
+
+type Admission struct {
+	Id            int
+	AdmissionDate string
+	DischargeDate string
+	PatientID     int
+}
+
+type Allocation struct {
+	Id          int `json:"id"`
+	AdmissionID int
+	EmployeeID  int
+	StartTime   string
+	EndTime     string
+}
+
+type Employee struct {
+	Id       int
+	Surname  string
+	Forename string
+}
+
+type Patient struct {
+	Id        int
+	Surname   string
+	Forename  string
+	NhsNumber string
+}
