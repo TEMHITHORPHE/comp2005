20ea815c2aeffcfa1aca40c1c7a351d91f5baf58
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-05-13 10:16:25 +0100

    Added Handler Implementation

--- mock_server/src/handlers/employee.go
+++ mock_server/src/handlers/employee.go
@@ -1,7 +1,36 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	model "phxlabs/m/comp2005/mockserver/src/models"
+)
 
-func An_Employee(w http.ResponseWriter, r *http.Request) {
+func An_Employee(response http.ResponseWriter, req *http.Request) {
+	pr("[An_Employee]: ", req.URL)
 
+	ID, err := extractID(req.URL.Path)
+	if req.Method == "GET" && err == nil {
+
+		ID_found := false
+		var Empl model.Employee
+		Employees := model.GetAllEmployees()
+
+		for _, employee := range Employees {
+			if employee.Id == ID {
+				Empl = employee
+				ID_found = true
+				break
+			}
+		}
+
+		if ID_found {
+			sendJSONRespose(response, Empl)
+		} else {
+			response.WriteHeader(http.StatusNotFound)
+		}
+
+	} else {
+		pr(err)
+		response.WriteHeader(403)
+	}
 }
