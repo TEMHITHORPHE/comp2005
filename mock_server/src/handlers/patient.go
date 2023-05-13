1209d43aff22b5f5f4b2aefedb60687884f6a4ab
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-05-13 10:16:34 +0100

    Added Handler Implementation

--- mock_server/src/handlers/patient.go
+++ mock_server/src/handlers/patient.go
@@ -1,7 +1,36 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	model "phxlabs/m/comp2005/mockserver/src/models"
+)
 
-func A_Patient(w http.ResponseWriter, r *http.Request) {
+func A_Patient(response http.ResponseWriter, req *http.Request) {
+	pr("[An_Patient]: ", req.URL)
 
+	ID, err := extractID(req.URL.Path)
+	if req.Method == "GET" && err == nil {
+
+		ID_found := false
+		var Patnt model.Employee
+		Patients := model.GetAllEmployees()
+
+		for _, patient := range Patients {
+			if patient.Id == ID {
+				Patnt = patient
+				ID_found = true
+				break
+			}
+		}
+
+		if ID_found {
+			sendJSONRespose(response, Patnt)
+		} else {
+			response.WriteHeader(404)
+		}
+
+	} else {
+		pr(err)
+		response.WriteHeader(403)
+	}
 }
