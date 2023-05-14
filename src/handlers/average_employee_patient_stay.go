67bca4ad0d56c68323df3bed873e951641e22059
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-05-14 10:12:50 +0100

    Handler Implementation init

--- src/handlers/average_employee_patient_stay.go
+++ src/handlers/average_employee_patient_stay.go
@@ -1,7 +1,33 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+	model "phxlabs/m/comp2005/automated_testing/src/models"
+)
 
 func EmployeePatientsDurationAverage(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == http.MethodGet {
+
+		employeeID, err := extractIDFromPath(r.URL.Path)
+		if err != nil {
+			pr(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		var patientsInfo []model.Patient
+		// Retrieve all Patients
+		err = GetEmployeePatients(employeeID, &patientsInfo, w)
+		if err != nil {
+			return
+		}
+
+		pr("PatientsAVG: ", patientsInfo)
+
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
 }
